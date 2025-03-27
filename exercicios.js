16c9e19d91809636d73f6f06cf5d46ee082b6de0
Davi Snaider Mendoza <davisnaider06@gmail.com>
2025-03-26 22:24:33 -0300

    Add files via upload

---
+++ exercicios.js
@@ -0,0 +1,18 @@
+//exercicio 1
+nome = prompt("Digite seu nome:");
+for(i=0;i<nome.length;i++){
+    console.log(nome[i]);
+};
+
+//exercicio 2
+nome = prompt("Digite seu nome:");
+for(i=0;i<nome.length;i = i+2){
+    console.log(nome[i]);
+};
+
+//exercicio 3 
+nros = [5,8,4,2,9,6,1,7,3]
+soma=0
+for(i=0;i<nros.length;i++;){
+    console.log(soma[i]);
+}
